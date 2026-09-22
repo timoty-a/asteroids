af58f5d8b4997dbd4440b9879c2402137067f79a
Timothy Adeyemo <timothyadeyemo99@gmail.com>
2026-09-22 13:17:57 +0100

    Add game loop and window handling

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame import display
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(0, 0, 0)
+        display.flip()
+    
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
