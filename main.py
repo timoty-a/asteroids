f04791350470273c9c0f2a0a921475568d5b5d80
Timothy Adeyemo <timothyadeyemo99@gmail.com>
2026-09-24 12:39:46 +0100

    add rotation and asteroids

--- main.py
+++ main.py
@@ -2,18 +2,40 @@ import pygame
 from pygame import display
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+
+
 def main():
     pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
         screen.fill("black")
-        display.flip()
+        for thing in drawable:
+            thing.draw(screen)
+        pygame.display.flip()
+
         dt = clock.tick(60) / 1000
         
     
